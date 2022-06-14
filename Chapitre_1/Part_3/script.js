91fb8161860c9817e1eb3da0b5d32fa258489cd5
maxlestage <maxlestage@icloud.com>
2022-06-14 20:39:04 +0200

    add gitignore

---
+++ Chapitre_1/Part_3/script.js
@@ -0,0 +1,13 @@
+console.log("Hello Three.js");
+console.log(THREE);
+
+// Scene
+const scene = new THREE.Scene();
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const mesh = new THREE.Mesh(geometry, material);
+
+// Ajout de la scène
+scene.add(mesh);
