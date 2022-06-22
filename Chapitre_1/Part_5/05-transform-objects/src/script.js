53bb8144418961dc9f007e2d8ed3e77e798fdcfd
maxlestage <maxlestage@icloud.com>
2022-06-22 11:58:07 +0200

    Chap_5 : ✅

--- Chapitre_1/Part_5/05-transform-objects/src/script.js
+++ Chapitre_1/Part_5/05-transform-objects/src/script.js
@@ -1,10 +1,14 @@
 import "./style.css";
 import * as THREE from "three";
 
-// Canvas
+/**
+ *Canvas
+ */
 const canvas = document.querySelector("canvas.webgl");
 
-// Scene
+/**
+ * Scene
+ */
 const scene = new THREE.Scene();
 
 /**
@@ -13,7 +17,33 @@ const scene = new THREE.Scene();
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
 const mesh = new THREE.Mesh(geometry, material);
+
+/**
+ * mesh.position.x = 0.7;
+ * mesh.position.y = -0.6;
+ * mesh.position.z = 1;
+ */
+mesh.position.set(0.7, -0.6, 1);
+
+/**
+ * mesh.scale.x = 2;
+ * mesh.scale.y = 0.25;
+ * mesh.scale.z = 0.5;
+ */
+mesh.scale.set(2, 0.25, 0.5);
+
+/**
+ * Position caméra Euler à éviter :
+ * mesh.rotation.x = Math.PI * 0.25;
+ * mesh.rotation.y = Math.PI * 0.25;
+ */
 scene.add(mesh);
+
+/**
+ * Axes Helper
+ */
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
 
 /**
  * Sizes
@@ -28,6 +58,8 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
+// camera.lookAt(new THREE.Vector3(0, -1, 0));
+camera.lookAt(mesh.position);
 scene.add(camera);
 
 /**
